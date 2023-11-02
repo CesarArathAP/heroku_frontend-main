e7d0cf92a117faf2e31dac83c80d0a5fa44f5727
CesarArathAP <1722110585@utectulancingo.edu.mx>
2023-11-02 17:10:51 -0600

    Update main.py otra vez

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask import Flask
-from fastapi.middleware.cors import CORSMiddleware
 from flask import render_template
 
 app = Flask(__name__)
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.route("/")
 def index():
